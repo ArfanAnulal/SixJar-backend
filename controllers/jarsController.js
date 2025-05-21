20f18632ffc41cc44de0e2fc69b348710fad94b8
Arfan Anulal <151611444+ArfanAnulal@users.noreply.github.com>
2025-05-21 20:58:11 +0530

    Update jarsController.js

--- controllers/jarsController.js
+++ controllers/jarsController.js
@@ -1,4 +1,5 @@
 const {getUserIdFromToken} = require('../utils/tokenHelpers')
+const User = require('../models/User');
 const sayBye = (req, res) => {
     const userId = getUserIdFromToken(req.user);
     res.send(`Bye from the jars controller! User ID: ${userId}`);
